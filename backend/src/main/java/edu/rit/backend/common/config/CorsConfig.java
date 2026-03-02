09b6374c60313f688b2acfbf58be3aa716aa441e
Laura Kentera <lk7653@rit.edu>
2026-03-02 10:35:38 +0100

    hotfix

--- backend/src/main/java/edu/rit/backend/common/config/CorsConfig.java
+++ backend/src/main/java/edu/rit/backend/common/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package edu.rit.competitivewordle.common.config;
+package edu.rit.backend.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
